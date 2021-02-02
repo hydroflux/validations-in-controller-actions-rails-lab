103d6bed3b0cc070ef6b35c01c283d21e5e8f160
Jack Hubert <jphubert93@gmail.com>
2021-02-01 17:40:17 -0700

    solved

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,15 +2,23 @@ class PostsController < ApplicationController
   before_action :set_post!, only: [:show, :edit, :update]
 
   def show
+    @post = set_post!
+
+    render json: @post
   end
 
   def edit
   end
 
   def update
-    @post.update(post_params)
+    @post.assign_attributes(post_params)
+    if @post.valid?
+      @post.update(post_params)
+      redirect_to post_path(@post)
+    else
+      render "edit"
+    end
 
-    redirect_to post_path(@post)
   end
 
   private
